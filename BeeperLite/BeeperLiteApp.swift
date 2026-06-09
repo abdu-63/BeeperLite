9490edaf982cf778305334a0556d645cef6f2d88
abdu-63 <abdur.palta@icloud.com>
2026-06-09 10:18:02 +0200

    Initial Commit

---
+++ BeeperLite/BeeperLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeperLiteApp.swift
+//  BeeperLite
+//
+//  Created by Abdu on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BeeperLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
